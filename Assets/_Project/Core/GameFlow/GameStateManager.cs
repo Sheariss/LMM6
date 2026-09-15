b38c55429eca20e72554bbdf04dd9d8e456ffb38
perpetuallyprogress <ktorres3238@gmail.com>
2026-09-14 21:46:05 -0400

    Basic Update to GameStateMananger

--- Assets/_Project/Core/GameFlow/GameStateManager.cs
+++ Assets/_Project/Core/GameFlow/GameStateManager.cs
@@ -1,31 +1,21 @@
 using Atlas.Core.SceneManagement;
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 namespace Atlas.Core.GameState
 {
-    /// <summary>
-    /// Maintains the current IIS game flow state and gameplay mode, coordinates high-level application flow.
-    /// </summary>
     public class GameStateManager : MonoBehaviour
     {
-        // Singleton Instance
         public static GameStateManager Instance { get; private set; }
 
-        //Getters
         // Internal State primitives
         public GameFlowState FlowState { get; private set; }
         public GameplayMode GameplayMode { get; private set; }
 
-        /// <summary>
-        /// Events raised after the Game Flow State or Gameplay Mode changes.
-        /// </summary>
+        // Events
         public event Action<GameFlowState, GameFlowState> FlowStateChanged;
         public event Action<GameplayMode, GameplayMode> GameplayModeChanged;
 
-        // Awaken Manager check for duplicate instances
         private void Awake()
         {
             if (Instance != null && Instance != this)
@@ -49,13 +39,6 @@ namespace Atlas.Core.GameState
         }
 
         // -------------------- PRIVATE PRIMITIVE OPERATIONS --------------------
-        // Setters
-        /// <summary>
-        /// Changes the current high-level application flow state and gameplay mode.
-        /// </summary>
-        ///  <remarks>
-        ///  Gameplay Mode may only be modified while gameplay is active.
-        ///  </remarks>
         private void SetFlowState(GameFlowState state)
         {
             if (FlowState == state)
@@ -68,6 +51,7 @@ namespace Atlas.Core.GameState
 
             FlowStateChanged?.Invoke(previousState, FlowState);
         }
+
         private void SetGameplayMode(GameplayMode mode)
         {
             if (FlowState != GameFlowState.Playing)
@@ -91,8 +75,6 @@ namespace Atlas.Core.GameState
             GameplayModeChanged?.Invoke(previousMode, GameplayMode);
         }
 
-        // Reset Primitives
-        // Question should we not have a seperate ResetGameState or such?
         public void ResetGameplayMode()
         {
             if (GameplayMode == GameplayMode.None)
@@ -107,11 +89,7 @@ namespace Atlas.Core.GameState
         }
 
         // -------------------- INTERNAL OPERATIONS --------------------
-
-        // Transition Permission Matrix
-        private bool IsTransitionAllowed(
-            GameFlowState current,
-            GameFlowState target)
+        private bool IsTransitionAllowed( GameFlowState current, GameFlowState target)
         {
             return current switch
             {
@@ -151,12 +129,17 @@ namespace Atlas.Core.GameState
             };
         }
 
-        private bool TryToEnterState(GameFlowState target, GameScene scene)
+        private bool TryToEnterState(GameFlowState target, GameScene scene, bool resetGameplayMode = false)
         {
             if (!IsTransitionAllowed(FlowState, target))
             {
                 LogInvalidTransition(target);
                 return  false;
+            }
+
+            if (resetGameplayMode)
+            {
+                ResetGameplayMode();
             }
 
             SetFlowState(target);
@@ -188,163 +171,77 @@ namespace Atlas.Core.GameState
             );
         }
 
-        /// <summary>
-        /// Delegates scene loading to SceneLoadManager.
-        /// </summary>
-        /// <param name="scene"></param>
         private void RequestSceneTransition(GameScene scene)
         {
             SceneLoadManager.Instance.LoadScene(scene);
         }
 
         // -------------------- PUBLIC OPERATIONS --------------------
-        /// <summary>
-        /// Transitions the application to the specified Game Flow State and requests
-        /// the corresponding scene when applicable.
-        /// </summary>
-        /// <param name="currentState">
-        /// The current Game Flow State used to validate whether the requested transition is permitted.
-        /// </param>
-
-
         // -------------------- Scene-Backed State Entry --------------------
-        // Enter[GameFlowState] Generic State Change + Scene Load
-        // Validates the requested transition, updates the Game Flow State,
-        // resets gameplay context when applicable, and requests the corresponding scene.
-
         public void EnterSplash()
         {
-            // Any valid source -> Splash State + Splash Scene
-            // Request Splash Load from SceneLoadManager
-            ResetGameplayMode();
-            TryToEnterState(GameFlowState.Splash, GameScene.Splash);
+            TryToEnterState(GameFlowState.Splash, GameScene.Splash, true);
         }
 
         public void EnterMainMenu()
         {
-            // Any valid source -> Main Menu State + Main Menu Scene
-            ResetGameplayMode();
-            TryToEnterState(GameFlowState.MainMenu, GameScene.MainMenu);
+            TryToEnterState(GameFlowState.MainMenu, GameScene.MainMenu, true);
         }
 
         public void EnterLoading()
         {
-            TryToEnterState(GameFlowState.Loading, GameScene.Loading);
+            TryToEnterState(GameFlowState.Loading, GameScene.Loading, false);
         }
 
         public void EnterPlaying()
         {
-            TryToEnterState(GameFlowState.Playing, GameScene.SOS);
+            TryToEnterState(GameFlowState.Playing, GameScene.SOS, false);
         }
 
         public void EnterCredits()
         {
-            // Any valid source -> Credits State + Credits Scene
-            ResetGameplayMode();
-            TryToEnterState(GameFlowState.Credits, GameScene.Credits);
+            TryToEnterState(GameFlowState.Credits, GameScene.Credits, true);
         }
-
-
-
-
-
-
 
         // -------------------- Specialized operations State Entry --------------------
         public void StartNewGame(int slot)
         {
             EnterLoading();
-
-            // Call the SaveManager.StartNewSaveFile or something
-
+            // TODO: Call the SaveManager.StartNewSaveFile or something
             EnterPlaying();
         }
 
-        // Continuing from an existing save file
         public void LoadGame(int slotId)
         {
             EnterLoading();
-
-            // SaveManager loads and restores slot.
-
+            // TODO: SaveManager loads and restores slot.
             EnterPlaying();
         }
-
 
         // -------------------- Presentation Coupled State Entry --------------------
         public void LoadGame()
         {
-            // Any valid source -> Loading Process (AKA NO SCENE CHANGE)
-            if (!IsTransitionAllowed(FlowState, GameFlowState.Loading))
-            {
-
-                return;
-            }
-
-            SetFlowState(GameFlowState.Loading);
-
-            // Active Scene stays loaded
-            // LoadingPanelController populates, redraws, shows, updates and hides the panel.
+            TryTransitionTo(GameFlowState.Loading);
+            // TO DO: Active Scene stays loaded > LoadingPanelController populates, redraws, shows, updates and hides the panel.
         }
 
-        public void PauseGame()
+        public void PauseGame() 
         {
-            // Playing -> Paused
-
-
-            // ENSURE THAT CAN ONLY BE ACTIVATED WHILE PLAYING
-            // PRESERVE GAMEPLAY MODE
-            // have to decide if the pause menu only exisits inthe SOS scene (perhaps credits has a similar "emu ,or they can jsut be skipps or fast fowarded"
-            // Actual visibility must be handed by the pause menu controller
-            // so call something like PauseMenuCoontroller.ShowPauseMenu
-
-            if (!IsTransitionAllowed(
-                FlowState,
-                GameFlowState.Paused))
-            {
-                LogInvalidTransition(GameFlowState.Paused);
-                return;
-            }
-
-            SetFlowState(GameFlowState.Paused);
-
-            // PauseMenuController reacts to the state change
-            // or is invoked through the presentation system.
+            TryTransitionTo(GameFlowState.Paused);
+            // TO DO: PauseMenuController reacts to the state change or is invoked through the presentation system.
         }
 
-        public void ResumeGame()
+        public void ResumeGame() 
         {
-            if (!IsTransitionAllowed(
-                FlowState,
-                GameFlowState.Playing))
-            {
-                LogInvalidTransition(GameFlowState.Playing);
-                return;
-            }
-
-            SetFlowState(GameFlowState.Playing);
-
-            // SOS scene remains loaded.
-            // PauseMenuController hides the panel.
+            TryTransitionTo(GameFlowState.Playing);
+            // TODO: SOS scene remains loaded. > PauseMenuController hides the panel.
         }
 
-        public void EndGame()
+        public void EndGame() 
         {
-            // Playing -> GameOver
-            if (!IsTransitionAllowed(
-                FlowState,
-                GameFlowState.GameOver))
-            {
-                LogInvalidTransition(GameFlowState.GameOver);
-                return;
-            }
-
-            SetFlowState(GameFlowState.GameOver);
-
-            // SOS scene remains loaded.
-            // GameOverPanelController shows the panel.
+            TryTransitionTo(GameFlowState.GameOver);
+            ResetGameplayMode();
+            // TODO: SOS scene remains loaded > GameOverPanelController shows the panel.
         }
-
-
     }
 }
