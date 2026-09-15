a548a795eee286622df4f30181137b784438ab91
perpetuallyprogress <ktorres3238@gmail.com>
2026-09-15 02:34:10 -0400

    GameSaveData added

---
+++ Assets/_Project/Core/Pesistance/Data/GameSaveData.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Atlas.Core.Persistence.Data
+{
+    [System.Serializable]
+    public sealed class GameSaveData
+    {
+        /*
+        public SaveMetadata metadata;
+        public SOSSaveData sos;
+        public PlayerSaveData player;
+        public InvestigationSaveData investigation;
+        public WorldSaveData world;
+        public ProgressionSaveData progression;
+        */
+    }
+}
