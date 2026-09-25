11f09da2672b3fda8760306679ba6975154ff81d
perpetuallyprogress <ktorres3238@gmail.com>
2026-09-25 04:14:03 -0400

    CMD Updated

---
+++ Assets/_Project/AuthoredData/Scripts/CMD/CMDCommandLibrary.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CMDCommandLibrary", menuName = "Scriptable Objects/CMDCommandLibrary")]
+public class CMDCommandLibrary : ScriptableObject
+{
+    
+}
